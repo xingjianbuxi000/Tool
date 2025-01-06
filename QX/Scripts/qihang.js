08e92a1b8dd613edf1c60957416627efc7532d89
xingjian <122657381+xingjianbuxi000@users.noreply.github.com>
2025-01-07 00:54:13 +0800

    更新 qihang.js

---
+++ QX/Scripts/qihang.js
@@ -0,0 +1,11 @@
+let body = $response.body; // 获取原始响应体
+let obj = JSON.parse(body); // 解析 JSON
+
+// 修改到期时间
+if (obj && obj.data) {
+  obj.data.deadlineEndTime = "2099-12-31 23:59:59"; // 自定义延长时间
+  obj.data.levelName = "星耀直通卡【永久】"; // 可选：修改会员名称
+}
+
+// 重新转换为 JSON 字符串并返回
+$done({ body: JSON.stringify(obj) });
